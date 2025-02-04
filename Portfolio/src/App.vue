2f481c0e518add90bc0143a03a11966c7bd83390
Thomas Taulo <taulo@live.dk>
2025-02-04 09:47:10 +0100

    init

---
+++ Portfolio/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!--  <Card></Card>-->
+
+  <!--  <div style="display: flex; height: 100%; align-items: center;">-->
+  <!--    <Button></Button>-->
+  <!--    <div style="margin: 10px;"></div>-->
+  <!--    <Input></Input>-->
+  <!--  </div>-->
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
